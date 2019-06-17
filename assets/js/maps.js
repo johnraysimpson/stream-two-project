7c7fecc852e7d82eefb2e60c96ec70596b41b9a6
John Simpson <johnnyraysimpson@hotmail.co.uk>
2019-06-17 20:39:28 +0000

    refactored javascript coding for repeated code, some small html and styling changes

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -4,18 +4,17 @@ var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/i
 var infoWindow;
 var hostnameRegexp = new RegExp('^https?://.+?/');
 
-
+// DEFINED FUNCTIONS
+//Initialise the map
 function initMap() {
     var startLoc = { lat: 46.619261, lng: -33.134766 };
     map = new google.maps.Map(
-        document.getElementById('map'), { 
-            zoom: 2, 
+        document.getElementById('map'), {
+            zoom: 2,
             center: startLoc,
             mapTypeControl: false,
             panControl: false
         });
-        
-    
 
     var defaultBounds = new google.maps.LatLngBounds(
         new google.maps.LatLng(-90, -180),
@@ -36,195 +35,50 @@ function initMap() {
     autocomplete = new google.maps.places.Autocomplete(input, options);
 
     autocomplete.addListener('place_changed', searchCity);
+    
+    //Function for autocompleting search field with cities, only executes if a city is selected
+    
+    function searchCity() {
+        var place = autocomplete.getPlace();
+        if (place.geometry) {
+            map.panTo(place.geometry.location);
+            map.setZoom(15);
+            $("#buttons").slideDown('slow');
+        }
+        else {
+            document.getElementById('autocomplete').placeholder = 'Enter a city';
+        }
+    }
 }
 
-function searchCity() {
-    var place = autocomplete.getPlace();
-    if (place.geometry) {
-        map.panTo(place.geometry.location);
-        map.setZoom(15);
-        $("#buttons").slideDown('slow');
-    }
-    else {
-        document.getElementById('autocomplete').placeholder = 'Enter a city';
-    }
+//Function to search within an area (given a type) and results are shown with markers (given a color for the type of attraction)
+
+function searchInArea(event) {
+    $("button > div").removeClass("click");
+    $(this).children("div").addClass("click");
+    var search = {
+        bounds: map.getBounds(),
+        types: [event.data.type]
+    };
+    clearMarkers();
+    places.nearbySearch(search, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                var markerIcon = `assets/images/icons8-map-pin-${event.data.color}.png`;
+                markers[i] = new google.maps.Marker({
+                    position: results[i].geometry.location,
+                    animation: google.maps.Animation.DROP,
+                    icon: markerIcon
+                });
+                markers[i].placeResult = results[i];
+                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+                setTimeout(dropMarker(i), i * 100);
+            }
+        }
+    });
 }
 
-$("#search-hotels").click(function() {
-    $("button > div").removeClass("click");
-    $(this).children("div").addClass("click");
-    var search = {
-        bounds: map.getBounds(),
-        types: ['lodging']
-    };
-    clearMarkers();
-    places.nearbySearch(search, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var markerIcon = 'assets/images/icons8-map-pin-green.png';
-                markers[i] = new google.maps.Marker({
-                    position: results[i].geometry.location,
-                    animation: google.maps.Animation.DROP,
-                    icon: markerIcon
-                });
-                markers[i].placeResult = results[i];
-                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-                setTimeout(dropMarker(i), i * 100);
-            }
-        }
-    });
-
-});
-
-$("#search-museum").click(function() {
-    $("button > div").removeClass("click");
-    $(this).children("div").addClass("click");
-    var search = {
-        bounds: map.getBounds(),
-        types: ['museum']
-    };
-    clearMarkers();
-    places.nearbySearch(search, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var markerIcon = 'assets/images/icons8-map-pin-red.png';
-                markers[i] = new google.maps.Marker({
-                    position: results[i].geometry.location,
-                    animation: google.maps.Animation.DROP,
-                    icon: markerIcon
-                });
-                markers[i].placeResult = results[i];
-                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-                setTimeout(dropMarker(i), i * 100);
-            }
-        }
-    });
-});
-
-$("#search-zoo").click(function() {
-    $("button > div").removeClass("click");
-    $(this).children("div").addClass("click");
-    var search = {
-        bounds: map.getBounds(),
-        types: ['zoo']
-    };
-    clearMarkers();
-    places.nearbySearch(search, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var markerIcon = 'assets/images/icons8-map-pin-red.png';
-                markers[i] = new google.maps.Marker({
-                    position: results[i].geometry.location,
-                    animation: google.maps.Animation.DROP,
-                    icon: markerIcon
-                });
-                markers[i].placeResult = results[i];
-                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-                setTimeout(dropMarker(i), i * 100);
-            }
-        }
-    });
-});
-
-$("#search-shopping").click(function() {
-    $("button > div").removeClass("click");
-    $(this).children("div").addClass("click");
-    var search = {
-        bounds: map.getBounds(),
-        types: ['shopping_mall']
-    };
-    clearMarkers();
-    places.nearbySearch(search, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var markerIcon = 'assets/images/icons8-map-pin-red.png';
-                markers[i] = new google.maps.Marker({
-                    position: results[i].geometry.location,
-                    animation: google.maps.Animation.DROP,
-                    icon: markerIcon
-                });
-                markers[i].placeResult = results[i];
-                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-                setTimeout(dropMarker(i), i * 100);
-            }
-        }
-    });
-});
-
-$("#search-restaurant").click(function() {
-    $("button > div").removeClass("click");
-    $(this).children("div").addClass("click");
-    var search = {
-        bounds: map.getBounds(),
-        types: ['restaurant']
-    };
-    clearMarkers();
-    places.nearbySearch(search, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var markerIcon = 'assets/images/icons8-map-pin-blue.png';
-                markers[i] = new google.maps.Marker({
-                    position: results[i].geometry.location,
-                    animation: google.maps.Animation.DROP,
-                    icon: markerIcon
-                });
-                markers[i].placeResult = results[i];
-                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-                setTimeout(dropMarker(i), i * 100);
-            }
-        }
-    });
-});
-
-$("#search-bar").click(function() {
-    $("button > div").removeClass("click");
-    $(this).children("div").addClass("click");
-    var search = {
-        bounds: map.getBounds(),
-        types: ['bar']
-    };
-    clearMarkers();
-    places.nearbySearch(search, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var markerIcon = 'assets/images/icons8-map-pin-blue.png';
-                markers[i] = new google.maps.Marker({
-                    position: results[i].geometry.location,
-                    animation: google.maps.Animation.DROP,
-                    icon: markerIcon
-                });
-                markers[i].placeResult = results[i];
-                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-                setTimeout(dropMarker(i), i * 100);
-            }
-        }
-    });
-});
-
-$("#search-cafe").click(function() {
-    $("button > div").removeClass("click");
-    $(this).children("div").addClass("click");
-    var search = {
-        bounds: map.getBounds(),
-        types: ['cafe']
-    };
-    clearMarkers();
-    places.nearbySearch(search, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var markerIcon = 'assets/images/icons8-map-pin-blue.png';
-                markers[i] = new google.maps.Marker({
-                    position: results[i].geometry.location,
-                    animation: google.maps.Animation.DROP,
-                    icon: markerIcon
-                });
-                markers[i].placeResult = results[i];
-                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-                setTimeout(dropMarker(i), i * 100);
-            }
-        }
-    });
-});
+//Function for clearing markers if markers alreay exist
 
 function clearMarkers() {
     for (var i = 0; i < markers.length; i++) {
@@ -235,11 +89,15 @@ function clearMarkers() {
     markers = [];
 }
 
+//Function for dropping markers on corresponding search results
+
 function dropMarker(i) {
     return function() {
         markers[i].setMap(map);
     };
 }
+
+//Function for displaying the info windowwhen a marker is clicked
 
 function showInfoWindow() {
     var marker = this;
@@ -253,10 +111,12 @@ function showInfoWindow() {
         });
 }
 
+//Function for the information gathered from the search result marker and formatted
+
 function buildIWContent(place) {
     document.getElementById('iw-icon').innerHTML = '<img style="height: 24px;" ' +
         'src="' + place.icon + '"/>';
-    document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url +
+    document.getElementById('iw-url').innerHTML = '<b><a target="_blank" href="' + place.url +
         '">' + place.name + '</a></b>';
     document.getElementById('iw-address').textContent = place.vicinity;
 
@@ -290,13 +150,25 @@ function buildIWContent(place) {
         var fullUrl = place.website;
         var website = hostnameRegexp.exec(place.website);
         if (website === null) {
-            website = 'http://' + place.website + '/';
+            website = 'http://' + fullUrl + '/';
             fullUrl = website;
         }
         document.getElementById('iw-website-row').style.display = '';
-        document.getElementById('iw-website').textContent = website;
+        document.getElementById('iw-website').innerHTML = '<b><a target="_blank" href="' + website + '">' + website + '</a></b>';
     }
     else {
         document.getElementById('iw-website-row').style.display = 'none';
     }
 }
+
+//CALLED FUNCTIONS
+//search hotels in map
+$("#search-hotels").click({type: 'lodging', color: 'green'}, searchInArea);
+//search attractions in map
+$("#search-museum").click({type: 'museum', color: 'red'}, searchInArea);
+$("#search-zoo").click({type: 'zoo', color: 'red'}, searchInArea);
+$("#search-shopping").click({type: 'shopping_mall', color: 'red'}, searchInArea);
+//search food and drink in map
+$("#search-restaurant").click({type: 'restaurant', color: 'blue'}, searchInArea);
+$("#search-bar").click({type: 'bar', color: 'blue'}, searchInArea);
+$("#search-cafe").click({type: 'cafe', color: 'blue'}, searchInArea);
